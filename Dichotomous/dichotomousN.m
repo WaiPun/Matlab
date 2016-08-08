9e45df8b2c18d380a1159116503e625042c6c803
WaiPun <wai.pun12@gmail.com>
2016-08-08 21:29:18 +0800

    Add files via upload

---
+++ Dichotomous/dichotomousN.m
@@ -0,0 +1,32 @@
+function [x,y] = dichotomousN(f,a,b,N)
+c = (a+b)/2;
+d = (a+c)/2;
+e = (c+b)/2;
+
+for i = 1:N
+if f(d) == min(f([c,d,e]))
+    b = c;
+    c = d;
+    d = (a+c)/2;
+    e = (c+b)/2;
+elseif f(c) == min(f([c,d,e]))
+    a = d;
+    b = e;
+    d = (a+c)/2;
+    e = (c+b)/2;
+else f(e) == min(f([c,d,e]))
+    a = c;
+    c = e; 
+    d = (a+c)/2;
+    e = (c+b)/2;
+end
+end
+[y,x] = min(f([d,c,e]));
+s = [d,c,e]; 
+x = s(x);
+
+        
+        
+        
+        
+        
